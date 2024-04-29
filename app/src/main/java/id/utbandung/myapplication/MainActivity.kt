99f21973f6db89d254fbd57059bee85ed5ab4e07
LOKALSF56 <asuryafirdaus@gmail.com>
2024-04-29 20:42:19 +0700

    sec push

--- app/src/main/java/id/utbandung/myapplication/MainActivity.kt
+++ app/src/main/java/id/utbandung/myapplication/MainActivity.kt
@@ -1,36 +1,39 @@
 package id.utbandung.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var txt1: TextView
-    private lateinit var button: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        txt1 = findViewById(R.id.txt1)
-        button = findViewById(R.id.button)
-
-        txt1.text = "Halo EUY"
-        button.setOnClickListener{
-            val intentDestination = Intent(this@MainActivity,MainActivity2::class.java)
-            //intentDestination.putExtra(MainActivity2.EXTRA_DATA,"Hello Ngabs")
-            intentDestination.putExtra(MainActivity2.EXTRA_DATA,mahasewa("Jajang","22521903"))
-            startActivity(intentDestination)
-        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val showButton: Button = findViewById(R.id.btnshow)
+        showButton.setOnClickListener{
+            showAlertDialog()
+        }
+    }
+    private fun showAlertDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Aquma")
+            .setMessage("Uninstall Aplikasi? ")
+            .setPositiveButton("yes"){ dialog, which ->
+            Toast.makeText(this,"Aplikasi Berhasil di Uninstall",Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("no"){dialog, which ->
+                dialog.dismiss()
+            }
+        val alertDialog: AlertDialog=builder.create()
+        alertDialog.show()
     }
 }
--- app/src/main/java/id/utbandung/myapplication/mahasewa.kt
+++
@@ -1,9 +0,0 @@
-package id.utbandung.myapplication
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class mahasewa(val name: String?,val nim: String?,):Parcelable{
-
-}
